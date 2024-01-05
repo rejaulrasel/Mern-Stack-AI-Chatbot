5896e0019550423c22c72120ac559d50ff5946c8
rejaul rasel <rejaulhasanrasel28@gmail.com>
2024-01-05 14:57:01 +0600

    routing the pages

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import { AppBar, Toolbar } from '@mui/material';
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <AppBar>
+                <Toolbar></Toolbar>
+            </AppBar>
+        </div>
+    );
+};
+
+export default Header;
