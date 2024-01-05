87245cf4b22237801526ea68c01bbd18d49688fd
rejaul rasel <rejaulhasanrasel28@gmail.com>
2024-01-05 15:33:53 +0600

    added logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,14 @@
-import { AppBar, Toolbar } from '@mui/material';
 import React from 'react';
+import { AppBar, Toolbar } from '@mui/material';
+import Logo from './shared/Logo';
 
 const Header = () => {
     return (
         <div>
-            <AppBar>
-                <Toolbar></Toolbar>
+            <AppBar sx={{bgcolor:"transparent", position:"static", boxShadow:"none"}}>
+                <Toolbar sx={{display:"flex"}}>
+                    <Logo/>
+                </Toolbar>
             </AppBar>
         </div>
     );
