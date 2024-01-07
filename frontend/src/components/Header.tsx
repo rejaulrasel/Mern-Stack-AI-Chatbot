a5f2d7f3a88d36bc97f5f849467ba24edcc50e50
rejaul rasel <rejaulhasanrasel28@gmail.com>
2024-01-07 10:35:45 +0600

    added context api more

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,54 @@
-import React from 'react';
-import { AppBar, Toolbar } from '@mui/material';
-import Logo from './shared/Logo';
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
 
 const Header = () => {
-    return (
-        <div>
-            <AppBar sx={{bgcolor:"transparent", position:"static", boxShadow:"none"}}>
-                <Toolbar sx={{display:"flex"}}>
-                    <Logo/>
-                </Toolbar>
-            </AppBar>
+  const auth = useAuth();
+  return (
+    <AppBar
+      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
+    >
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo />
+        <div style={{display:"flex"}}>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/chat"
+                text="Go To Chat"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/"
+                text="logout"
+                onClick={auth.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/login"
+                text="Login"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/signup"
+                text="Signup"
+              />
+            </>
+          )}
         </div>
-    );
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
