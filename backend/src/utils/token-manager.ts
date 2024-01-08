8a445bffde86bdf09605590ffe6cc4adabfb2109
rejaul rasel <rejaulhasanrasel28@gmail.com>
2024-01-08 10:45:31 +0600

    verify user token

--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -1,9 +1,20 @@
+import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import { COOKIE_NAME } from "./constants.js";
 
 export const createToken = (id: string, email: string, expiresIn: string) => {
-    const paylod = { id, email };
-    const token = jwt.sign(paylod, process.env.JWT_SECRET, {
+    const payload = { id, email };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn,
     });
     return token;
+};
+
+export const verifyToken = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const token = req.signedCookies[`${COOKIE_NAME}`];
+    console.log(token);
 };
