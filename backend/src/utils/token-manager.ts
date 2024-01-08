fc370cd906db6eac3d508607878ad15eac03e6f3
rejaul rasel <rejaulhasanrasel28@gmail.com>
2024-01-08 11:20:26 +0600

    user token check

--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -16,5 +16,20 @@ export const verifyToken = async (
     next: NextFunction
 ) => {
     const token = req.signedCookies[`${COOKIE_NAME}`];
-    console.log(token);
+    if (!token || token.trim() === "") {
+        return res.status(401).json({ message: "Token not Received" })
+    }
+    return new Promise<void>((resolve, reject) => {
+        return jwt.verify(token, process.env.JWT_SECRET, (err, success) => {
+            if (err) {
+                reject(err.message);
+                return res.status(401).json({ message: "Token Expired" })
+            } else {
+                console.log('Token Verification Successful');
+                resolve();
+                res.locals.jwtData = success;
+                return next();
+            }
+        });
+    });
 };
